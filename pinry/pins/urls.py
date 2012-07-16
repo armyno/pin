f8deb5cbe596f71089e364e36d4934d0ecdcda23
Armyno <dinh.duc07@gmail.com>
2012-07-16 10:46:07 -0700

    Revert "Signed-off-by: Armyno <dinh.duc07@gmail.com>"
    This reverts commit 8b6b7571ed77bed951b89a78d55a1ee5cb71c9ec.

--- pinry/pins/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-
-urlpatterns = patterns('pinry.pins.views',
-    url(r'^$', 'recent_pins', name='recent-pins'),
-    url(r'^new-pin/$', 'new_pin', name='new-pin'),
-)
